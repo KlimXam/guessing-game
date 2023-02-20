3aeebc2880a7412058ac8644af575ff51b588730
KlimXam <apecypc@gmail.com>
2023-02-20 16:36:28 +0300

    feat: guess

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+    this.min = min;
+    this.max = max;
     }
 
     guess() {
-
+    this.gues = Math.round((this.max + this.min) /2)
+    return this.gues;
     }
 
     lower() {
-
+    this.max = this.gues
     }
 
     greater() {
-
+    this.min = this.gues
     }
 }
 
